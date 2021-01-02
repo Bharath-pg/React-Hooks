96eb2d4f6e6cc7c1ca61313e081002f284685359
unknown <bharathpg1997@gmail.com>
2021-01-02 17:58:56 +0530

    optimised code

--- src/App.js
+++ src/App.js
@@ -1,63 +1,99 @@
 import React, { useState } from 'react';
 
+
+const fruits = ["Apple", "Mango", "Banana"];
+
 const prices = {
   Apple: 20,
   Mango: 15,
+  Banana: 5,
 };
 
 const taxPercent = 2;
 
+const initialState = {};
+
+fruits.forEach((fruit) => {
+  const price = prices[fruit];
+  initialState[fruit] = {
+    count: 0,
+    price,
+  };
+});
+
+
+
 function App() {
+  
+  const [basket, setBasket] = useState(initialState);
 
-  const [applesCount, setApplesCount] = useState(0);
-  const [mangoesCount, setMangoesCount] = useState(0);
+  let totalFruitsCount = 0;
+  let totalFruitsPrice = 0;
 
-  const totalFruitsCount = applesCount + mangoesCount;
-  const totalFruitsPrice = applesCount * prices.Apple + mangoesCount * prices.Mango;
+  Object.keys(basket).forEach((fruitKey) => {
+    const fruit = basket[fruitKey];
+    totalFruitsCount += fruit.count;
+    totalFruitsPrice += fruit.count * fruit.price;
+  });
 
   const taxPrice = (totalFruitsPrice * taxPercent) / 100;
   const totalPrice = totalFruitsPrice + taxPrice;
-  function addApplesToBasket() {
-    setApplesCount(applesCount + 1);
+
+
+
+  function addFruitToBasket(fruitKey) {
+    
+    setBasket({
+      ...basket, 
+      [fruitKey]: {
+        ...basket[fruitKey], 
+        count: basket[fruitKey].count + 1,
+      },
+    });
+    }
+
+  function removeFruitFromBasket(fruitKey) {
+    setBasket({
+      ...basket, 
+      [fruitKey]: {
+        ...basket[fruitKey], 
+        count: basket[fruitKey].count - 1,
+      },
+    });
   }
 
-  function removeApplesFromBasket() {
-    setApplesCount(applesCount - 1);
-  }
 
-  function addMangoesToBasket() {
-    setMangoesCount(mangoesCount + 1);
-  }
-
-  function removeMangoesFromBasket() {
-    setMangoesCount(mangoesCount - 1);
-  }
 
   return (
     <div>
+     
      <h1>Fruit Shop</h1>
-     <div>
-     <button onClick = {addApplesToBasket}> + </button>&emsp;
-     <button onClick = {removeApplesFromBasket}> - </button>&emsp;
-     Apple: {applesCount}
-     </div>
-      <br />
+     {Object.keys(basket).map((fruitKey) => {
+       const fruit = basket[fruitKey];
+       return(
+         <div>
+         
+      
+            <button onClick = {() => addFruitToBasket(fruitKey)}> + </button>&emsp;
+            <button onClick = {() => removeFruitFromBasket(fruitKey)}> - </button>&emsp;
+     {fruitKey} = {fruit.count}
+   
+         </div>
+       );
+     })}
+     
+  
 
-     <div>
-     <button onClick = {addMangoesToBasket}> + </button>&emsp;
-     <button onClick = {removeMangoesFromBasket}> - </button>&emsp;
-     Mango: {mangoesCount}
-     </div>
-
+    
      <hr />
      <h1>Invoice</h1>
      <div>Total no of fruits: {totalFruitsCount}</div>
      <div>Total:{totalFruitsPrice} </div>
-     <div>Tax: 2%</div>
+     <div>Tax: {taxPercent}</div>
      <div>Tax Price: {taxPrice}</div>
      <br />
      <div>Grand Total: {totalPrice}</div>
-
+    
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Container } from 'react-bootstrap'
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Container>
+     
     <App />
+  
+    </Container>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
