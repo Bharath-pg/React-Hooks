026c44614adf3c8a546fe24a297b074f34c36e2b
unknown <bharathpg1997@gmail.com>
2021-01-02 11:06:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const prices = {
+  Apple: 20,
+  Mango: 15,
+};
+
+const taxPercent = 2;
 
 function App() {
+
+  const [applesCount, setApplesCount] = useState(0);
+  const [mangoesCount, setMangoesCount] = useState(0);
+
+  const totalFruitsCount = applesCount + mangoesCount;
+  const totalFruitsPrice = applesCount * prices.Apple + mangoesCount * prices.Mango;
+
+  const taxPrice = (totalFruitsPrice * taxPercent) / 100;
+  const totalPrice = totalFruitsPrice + taxPrice;
+  function addApplesToBasket() {
+    setApplesCount(applesCount + 1);
+  }
+
+  function removeApplesFromBasket() {
+    setApplesCount(applesCount - 1);
+  }
+
+  function addMangoesToBasket() {
+    setMangoesCount(mangoesCount + 1);
+  }
+
+  function removeMangoesFromBasket() {
+    setMangoesCount(mangoesCount - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>Fruit Shop</h1>
+     <div>
+     <button onClick = {addApplesToBasket}> + </button>&emsp;
+     <button onClick = {removeApplesFromBasket}> - </button>&emsp;
+     Apple: {applesCount}
+     </div>
+      <br />
+
+     <div>
+     <button onClick = {addMangoesToBasket}> + </button>&emsp;
+     <button onClick = {removeMangoesFromBasket}> - </button>&emsp;
+     Mango: {mangoesCount}
+     </div>
+
+     <hr />
+     <h1>Invoice</h1>
+     <div>Total no of fruits: {totalFruitsCount}</div>
+     <div>Total:{totalFruitsPrice} </div>
+     <div>Tax: 2%</div>
+     <div>Tax Price: {taxPrice}</div>
+     <br />
+     <div>Grand Total: {totalPrice}</div>
+
     </div>
   );
 }
